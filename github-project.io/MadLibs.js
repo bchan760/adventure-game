d2a24e0c7fac9256d2898f4d3254c0801a67ab06
Bill <bchan760@gmail.com>
2019-03-04 17:59:35 -0800

    A Rebellious Phase

---
+++ github-project.io/MadLibs.js
@@ -0,0 +1,13 @@
+let name = window.prompt('What is your name?');
+
+window.alert("Welcome to the funniest Mad Lib story ever!");
+
+var name0 = window.prompt('What animal do you want to be?');
+document.getElementById('animal').innerText = name0;
+
+var name1 = window.prompt('What action do you want to do?')
+document.getElementById('action').innerText = name1;
+
+var name2 = window.prompt("what do you want your name to be?")
+document.getElementById('name').innerText = name0;
+
